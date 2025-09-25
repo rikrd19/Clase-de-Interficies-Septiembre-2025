1e4d96fb28caa3755c70efe2b539252c6824ea1c
Ricardo Avila <ricardoavilabazalar@gmail.com>
2025-09-25 16:15:39 +0200

    Initial commit: organize files in DisenoWeb folder

---
+++ laboratorio1/Actividad2/script.js
@@ -0,0 +1,22 @@
+let numero = 10; // number
+let texto = "Hola"; // string
+let activo = true; // boolean
+
+let suma = 10 + 3;
+let resta = 10 - 3;
+let producto = 10 * 3;
+let division = 10 / 3;
+let modulo = 10 % 3;
+
+console.log("numero:", numero);
+console.log("texto:", texto);
+console.log("activo:", activo);
+
+console.log("suma:", suma);
+console.log("resta:", resta);
+console.log("producto:", producto);
+console.log("division:", division);
+console.log("modulo:", modulo);
+
+let concatenado = texto + 5;
+console.log("concatenado:", concatenado);
